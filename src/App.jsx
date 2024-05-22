2d018aab987a41a797920865cf8f191a8a2ab63e
Shivaram2151 <137429576+Shivaram2151@users.noreply.github.com>
2024-05-22 18:22:18 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 import { Route, Router, Routes } from "react-router-dom";
-import { loadStripe } from "@stripe/stripe-js";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Transaction from "./components/Transaction/Transaction";
 import NotFound from "./components/NotFound";
@@ -15,7 +14,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         {/* <Route path="/checkout/:sessionId" element={<CheckoutPage />} /> */}
         <Route path="/success" element={<Transaction />} />
-        <Route path="/error" element={<NotFound />} />
+        <Route path="/Error" element={<NotFound />} />
       </Routes>
       {/* <LandingPage /> */}
     </>
