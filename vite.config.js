27f5836a71d3527b0dfc501fa86e6b922e19ceeb
Shivaram2151 <137429576+Shivaram2151@users.noreply.github.com>
2024-05-22 23:42:19 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Adjust the base path if your app is hosted in a subdirectory
+  base: "/*", // Adjust the base path if your app is hosted in a subdirectory
   build: {
     assetsDir: "assets", // Ensure assets are placed in the 'assets' directory
     rollupOptions: {
